e3b7f147a28b7494f4bb93b350ffc75d8d08ed1d
Jiwon <moonlight_youlike@naver.com>
2023-03-24 19:56:29 +0900

    Solved 0-1 knapsack problem

---
+++ jiwon/12865.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+# 물품의 수, 버틸 수 있는 무게
+n, k = map(int, input().split())
+
+# 무게, 가치
+graph = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]
+
+# 가방 리스트
+bag = [[0] * (k+1) for _ in range(n+1)]
+
+# i : 물건, j : 가방
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        # 무게, 가치
+        weight, value = graph[i][0], graph[i][1]
+
+        if j < weight:
+            # weight보다 작으면 위의 값 그대로 가져옴
+            bag[i][j] = bag[i-1][j]
+        else:
+            # 1) 물건을 넣은 뒤 남은 무게를 채울 수 있는 최댓값을 위의 행에서 가져와 더해줌
+            # 2) 현재 물건 말고 다른 물건들로 채움
+            # 1, 2번 중에 큰 값을 넣음
+
+            # max(현재 물건 가치 + bag[이전 물건][현재 가방 무게 - 현재 물건 무게], bag[이전 물건][현재 가방 무게])
+            bag[i][j] = max(value + bag[i-1][j-weight], bag[i-1][j])
+
+print(bag[n][k])
