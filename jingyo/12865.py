87dabd7c09aeb2b4aa50163752f33e3a38b01a70
jinkyoJB <wlsry0910@gmail.com>
2023-03-24 23:39:28 +0900

    feat(12865.py): add solution for the problem '평범한 배낭' feat(9251.py): add solution for the problem 'LCS'

---
+++ jingyo/12865.py
@@ -0,0 +1,23 @@
+#평범한 배낭
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+datas = [[0, 0]]  # [[0, 0], [6, 13], [4, 8], [3, 6], [5, 12]]
+for _ in range(n):
+    w, v = map(int, input().split())
+    datas.append([w,v])
+
+dt = [[0]*(k+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        r = j-datas[i][0]  # '탐색하는 무게'-'탐색하는 물건의 무게'의 차를 기준으로 판별하자
+        if r>=0:
+            # 왼쪽은 현재탐색중인 물건을 넣지 않았을 경우 vs 현재 탐색중인 물건을 넣을 경우(현재 탐색중 물건의 가치+ 넣지않았을 때 나머지무게에 맞는 가치)
+            dt[i][j] = max(datas[i][1] + dt[i-1][r], dt[i-1][j])
+        else: #탐색하는 물건을 담고 남은 무게량이 있으면
+            dt[i][j] = dt[i-1][j]
+print(dt[-1][-1])
+
--- jingyo/9251.py
+++ jingyo/9251.py
@@ -2,8 +2,16 @@
 import sys
 input = sys.stdin.readline
 
-a = str(input())
-b = str(input())
+a = str(input().rstrip())
+b = str(input().rstrip())
+len_a = len(a)
+len_b = len(b)
+dt = [[0]*(len_a+1) for _ in range(len_b+1)]
 
-dp = [0]*1001
-
+for i in range(1,len_b+1):
+    for j in range(1,len_a+1):
+        if b[i-1] == a[j-1]:
+            dt[i][j] = dt[i-1][j-1] +1
+        else:
+            dt[i][j] = max(dt[i-1][j],dt[i][j-1])
+print(dt[-1][-1])
