2e2d920bd059873177d475a9c79724d511aff20f
minseok1109 <park485201@naver.com>
2023-03-28 10:42:45 +0900

    feat: add two new python files
    Add two new python files, 1700.py and 1946.py, to the minseok directory.
    
    1700.py contains a solution to a problem that involves finding the minimum number of times a plug must be unplugged and reinserted into a power strip in order to use all the appliances.
    
    1946.py contains a solution to a problem that involves finding the number of applicants who can be hired given their scores on two tests.

---
+++ minseok/1946.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+answer = []
+T = int(input())
+for _ in range(T):
+    result = 1
+    n = int(input())
+    scores = sorted(
+        [tuple(map(int, input().split())) for _ in range(n)], key=lambda x: x[0]
+    )
+
+    top = scores[0]
+    for i in range(1, n):
+        if (top[0] < scores[i][0]) ^ (top[1] < scores[i][1]):
+            result += 1
+            top = scores[i]
+            continue
+
+    answer.append(result)
+
+for i in answer:
+    print(i)
